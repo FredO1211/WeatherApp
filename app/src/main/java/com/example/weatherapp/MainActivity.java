9a4aab83dfb6a44d4dd623ced9051bf27511594d
Radoslaw Fedor <radoslaw.fedor@gmail.com>
2020-03-04 13:25:44 +0100

    -Creating connection

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -15,9 +18,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
 
-    private TextView temperatureTextView;
+public class MainActivity extends AppCompatActivity implements FetchData.OnDownloadComplete {
+    private static final String TAG = "MainActivity";
+
+    public static TextView temperatureTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,36 +31,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         temperatureTextView=findViewById(R.id.temperatureTextView);
-        findWeather();
+        Button buttonParse = findViewById(R.id.parseButton);
+
+        FetchData process = new FetchData(this);
+        process.execute("https://api.openweathermap.org/data/2.5/weather?q=Sanok&appid=da04e8d884c2338e773b27e31ebd3e93&units=metric");
+    }
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status){
+        if(status==DownloadStatus.OK){
+            Log.d(TAG, "onDownloadComplete: data is "+data);
+        } else {
+            Log.e(TAG, "onDownloadComplete: failed with status"+status);
+        }
     }
 
-    public void findWeather(){
-        String url="http://api.openweathermap.org/data/2.5/weather?q=Sanok&appid=da04e8d884c2338e773b27e31ebd3e93&units=metric";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONObject jsonObject = response.getJSONObject("main");
-                    int temp = (int) jsonObject.getDouble("temp");
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    System.out.println("nie dzila");
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        RequestQueue queue= Volley.newRequestQueue(this);
-        queue.add(jsonObjectRequest);
-        setTemperature(20);
-    }
-    public void setTemperature(int temperature){
-        temperatureTextView.setText(temperature+"°C");
-    }
 }
